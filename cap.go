373a3e9e7529989f064c6b9c2b58ace81ea0812a
unknown <greensBurst@163.com>
2019-08-05 10:23:55 +0800

    新增post模块

--- cap.go
+++ cap.go
@@ -1,37 +1,87 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
 var (
 	g string
+	p string
 )
 
 func init() {
 	flag.StringVar(&g, "g", "get request url", "GET()请求地址")
+	flag.StringVar(&p, "p", "post request url", "POST()请求地址")
+	flag.Parse()
 }
 
 func main() {
-	flag.Parse()
 
 	if g != "get request url" {
-		res, err := http.Get(g)
-		deal(err)
-		defer res.Body.Close()
+		req, res := newreq("GET", g, nil)
+		request(req)
 		response(res)
-		request(res.Request)
+	} else if p != "post request url" {
+		form := make(map[string]string, 0)
+		fmt.Println("input key:value in one line,press over to break.")
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			if scanner.Text() == "over" {
+				break
+			}
+			kv := make([]string, 2, 2)
+			kv = strings.Split(scanner.Text(), ":")
+			form[kv[0]] = kv[1]
+		}
+
+		info := makeParameter(form)
+		req, res := newreq("POST", p, info)
+		request(req)
+		response(res)
 	}
+}
+
+func makeParameter(form map[string]string) io.Reader {
+	var str string
+	cnt := 0
+	for k, v := range form {
+		each := k + "=" + v
+		if cnt == 0 {
+			str = each
+		} else {
+			str = str + "&" + each
+		}
+		cnt++
+	}
+	info := strings.NewReader(str)
+	return info
 }
 
 func deal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func newreq(method string, url string, parameter io.Reader) (*http.Request, *http.Response) {
+	req, err := http.NewRequest(method, url, parameter)
+	deal(err)
+	if method == "POST" {
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	}
+	res, err := http.DefaultClient.Do(req)
+	deal(err)
+	defer res.Body.Close()
+	// body, _ := ioutil.ReadAll(res.Body)
+	// fmt.Println(string(body))
+	return req, res
 }
 
 func request(req *http.Request) {
@@ -58,10 +108,10 @@ func response(res *http.Response) {
 	fmt.Printf("\tMethod:\t\t%v\n\tStatus:\t\t%v\n\tProtocol:\t%v\n", res.Request.Method, res.Status, res.Proto)
 
 	fmt.Println("\nresponse Header:")
-	for k, v := range res.Header {
+	for k, v := range res.Header { //v []string
 		fmt.Printf("\t%v:\n", k)
 		for i := 0; i < len(v); i++ {
-			s := strings.Split(v[i], "; ")
+			s := strings.Split(v[i], "; ") //i string    s []string
 			for _, j := range s {
 				fmt.Printf("\t\t\t\t%v\n", j)
 			}
